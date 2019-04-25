cf7867ddc6f2da139af1649e06053e9f0c6fb477
Richard Le <richardle1997@gmail.com>
2019-04-24 18:06:18 -0700

    Added some scripts to test touch and click controls

---
+++ Assets/Scripts/Richard Scripts/FollowCam.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    public float followSpeed;
+    private CameraHolder cameraHolder;
+    
+    private void Awake()
+    {
+        cameraHolder = GameObject.FindGameObjectWithTag("CameraHolder").GetComponent<CameraHolder>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, cameraHolder.transform.position, followSpeed * Time.deltaTime);
+    }
+}
