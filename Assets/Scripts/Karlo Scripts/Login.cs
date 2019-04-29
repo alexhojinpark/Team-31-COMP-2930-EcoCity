a48245deb9b81c990b09ac12df3d0ced4d26e711
Karlo Krakan <karlo@krakan.ca>
2019-04-28 19:34:54 -0700

    basic login/sign up ui skeleton

---
+++ Assets/Scripts/Karlo Scripts/Login.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Text.RegularExpressions;
+
+public class Login : MonoBehaviour
+{
+    public GameObject username;
+    public GameObject password;
+    private string Username;
+    private string Password;
+    private string form;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (username.GetComponent<InputField>().isFocused)
+            {
+                password.GetComponent<InputField>().Select();
+            }
+        }
+        Username = username.GetComponent<InputField>().text;
+        Password = password.GetComponent<InputField>().text;
+    }
+}
---
+++ Assets/Scripts/Karlo Scripts/Signup.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Text.RegularExpressions;
+
+public class Signup : MonoBehaviour
+{
+    public GameObject username;
+    public GameObject password;
+    public GameObject confirmPassword;
+    private string Username;
+    private string Password;
+    private string ConfirmPassword;
+    private string form;
+    private bool PasswordValid = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void RegisterButton()
+    {
+        Debug.Log("Registration Successful");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (username.GetComponent<InputField>().isFocused)
+            {
+                password.GetComponent<InputField>().Select();
+            }
+            if (password.GetComponent<InputField>().isFocused)
+            {
+                confirmPassword.GetComponent<InputField>().Select();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (Password != "" && ConfirmPassword != "" && Username != "") {
+                RegisterButton();
+            }
+        }
+        Username = username.GetComponent<InputField>().text;
+        Password = password.GetComponent<InputField>().text;
+        ConfirmPassword = confirmPassword.GetComponent<InputField>().text;
+    }
+}
