a4ef04871d8887dd50cf42f6a203e97557cb362d
KristianPostnikoff <kris.postnikoff@gmail.com>
2019-04-30 14:33:51 -0700

    separated build menus

--- Assets/Scripts/Richard Scripts/UI/BuildMenu.cs
+++ Assets/Scripts/Richard Scripts/UI/BuildMenu.cs
@@ -9,37 +9,38 @@ public class BuildMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void SetButtonVisibilitySize(Plot.PlotSize size)
     {
-        switch (size)
-        {
-            case Plot.PlotSize.Large:
-                buildOptions[0].interactable = false;
-                buildOptions[1].interactable = false;
-                buildOptions[2].interactable = true;
-                break;
-            case Plot.PlotSize.Medium:
-                buildOptions[0].interactable = false;
-                buildOptions[1].interactable = true;
-                buildOptions[2].interactable = false;
-                break;
-            case Plot.PlotSize.Small:
-                buildOptions[0].interactable = true;
-                buildOptions[1].interactable = false;
-                buildOptions[2].interactable = false;
-                break;
-            default:
-                break;
-        }
+        //    switch (size)
+        //    {
+        //        case Plot.PlotSize.Large:
+        //            buildOptions[0].interactable = false;
+        //            buildOptions[1].interactable = false;
+        //            buildOptions[2].interactable = true;
+        //            break;
+        //        case Plot.PlotSize.Medium:
+        //            buildOptions[0].interactable = false;
+        //            buildOptions[1].interactable = true;
+        //            buildOptions[2].interactable = false;
+        //            break;
+        //        case Plot.PlotSize.Small:
+        //            buildOptions[0].interactable = true;
+        //            buildOptions[1].interactable = false;
+        //            buildOptions[2].interactable = false;
+        //            break;
+        //        default:
+        //            break;
+        //    }
+        //}
+
     }
-    
 }
