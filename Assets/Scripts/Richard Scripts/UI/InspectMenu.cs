71e526906cbfab5f3d56f6156c4afa80fdcc94ff
Richard Le <richardle1997@gmail.com>
2019-05-02 13:14:53 -0700

    For alex

---
+++ Assets/Scripts/Richard Scripts/UI/InspectMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InspectMenu : MonoBehaviour
+{
+    public Text nameText;
+    public Text description;
+    public List<Text> statTexts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetNameText(string s)
+    {
+        nameText.text = s;
+    }
+
+    public void SetDescriptionText(string s)
+    {
+        description.text = s;
+    }
+
+    public void SetStatTexts(string[] s)
+    {
+        for (int i = 0; i < statTexts.Count; i++)
+        {
+            statTexts[i].text = s[i];
+        }
+    }
+}
