30fad5ce0b07c27d6efbbd405ecd2fe5f7fdf007
Erik Golke <erikgolke@gmail.com>
2019-05-03 01:07:14 -0700

    Worked on saving tiles in a 2D array

--- Assets/Scripts/Erik Scripts/BuyTile.cs
+++ Assets/Scripts/Erik Scripts/BuyTile.cs
@@ -4,14 +4,32 @@ using UnityEngine;
 
 public class BuyTile : MonoBehaviour
 {
+    private const int rowNumber = 3;
+    private const int colNumber = 3;
+    private GameObject[,] tiles = new GameObject[rowNumber, colNumber];
+
     public GameObject[] hiddenTiles;
     // Start is called before the first frame update
     void Start()
     {
-        hiddenTiles = GameObject.FindGameObjectsWithTag("HiddenTile");
+        hiddenTiles = GameObject.FindGameObjectsWithTag("WorldTile");
         foreach (GameObject hiddenTile in hiddenTiles)
         {
             hiddenTile.SetActive(false);
+        }
+
+        foreach (GameObject hiddenTile in hiddenTiles) { 
+        for (int row = 0; row < rowNumber; row++)
+        {
+            for (int col = 0; col < colNumber; col++)
+            {
+                tiles[row, col] = hiddenTile;;
+                }
+        }
+            foreach (GameObject tile in tiles)
+            {
+                Debug.Log(tile);
+            }
         }
     }
 
@@ -19,5 +37,12 @@ public class BuyTile : MonoBehaviour
     void Update()
     {
         
+    }
+    private void handleClick()
+    {
+        foreach (GameObject hiddenTile in hiddenTiles)
+        {
+            hiddenTile.SetActive(false);
+        }
     }
 }
