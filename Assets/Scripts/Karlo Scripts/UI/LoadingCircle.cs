e8b99162c42440798cc87109ea31d3cc67bcd5ea
Karlo Krakan <karlo@krakan.ca>
2019-05-03 16:41:42 -0700

    leaderboard menue skeleton and items prefab

---
+++ Assets/Scripts/Karlo Scripts/UI/LoadingCircle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingCircle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(4);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * Time.deltaTime * -100);
+    }
+}
