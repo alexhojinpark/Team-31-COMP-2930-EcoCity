5fd96503636eec69e0688cb9c989bfeabb02f451
Erik Golke <erikgolke@gmail.com>
2019-05-03 00:04:35 -0700

    Added very basic grid system around main tile

---
+++ Assets/Scripts/Erik Scripts/BuyTile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuyTile : MonoBehaviour
+{
+    public GameObject[] hiddenTiles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hiddenTiles = GameObject.FindGameObjectsWithTag("HiddenTile");
+        foreach (GameObject hiddenTile in hiddenTiles)
+        {
+            hiddenTile.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
