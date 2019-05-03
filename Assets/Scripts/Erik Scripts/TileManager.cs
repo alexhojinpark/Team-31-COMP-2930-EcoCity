d18cdab0eb3df8b33c266ecb0a8d8b90c5c35d46
Erik Golke <erikgolke@gmail.com>
2019-05-03 15:26:34 -0700

    Added functionality to store world tiles in 2D array

---
+++ Assets/Scripts/Erik Scripts/TileManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    private const int rowNumber = 3;
+    private const int colNumber = 3;
+    public GameObject[,] tiles = new GameObject[rowNumber, colNumber];
+
+    public GameObject[] worldTiles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        worldTiles = GameObject.FindGameObjectsWithTag("WorldTile");
+        int count = 0;
+        for (int row = 0; row < rowNumber; row++)
+        {
+            for (int col = 0; col < colNumber; col++)
+            {
+                tiles[row, col] = worldTiles[count];
+                count++;
+            }
+        }
+        foreach (GameObject tile in tiles)
+        {
+            Debug.Log(tile);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
