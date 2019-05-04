ab91bc6f8cb6aaea4ff82d7bb4d2ccc94686110b
Richard Le <richardle1997@gmail.com>
2019-05-03 17:43:06 -0700

    Richard pulls everything

---
+++ Assets/Scripts/Richard Scripts/Buildings/ModelPicker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelPicker : MonoBehaviour
+{
+    public List<GameObject> models;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PickModel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void PickModel()
+    {
+        int randIndex = Random.Range(0, models.Count - 1);
+        GameObject myModel = Instantiate(models[randIndex], transform.parent.position, transform.parent.rotation, transform.parent);
+    }
+}
