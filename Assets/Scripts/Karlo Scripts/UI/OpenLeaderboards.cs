70f331d686942e3b5651cefa3d8be3926c51912e
Karlo Krakan <karlo@krakan.ca>
2019-05-08 17:49:58 -0700

    inserted menus into game

---
+++ Assets/Scripts/Karlo Scripts/UI/OpenLeaderboards.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLeaderboards : MonoBehaviour
+{
+    public GameObject GameCanvas;
+    public GameObject Leaderboard;
+
+    public void ShowLeaderboard() {
+        (Instantiate(Leaderboard) as GameObject).transform.parent = GameCanvas.transform;
+    }
+}
---
+++ Assets/Scripts/Karlo Scripts/UI/OpenSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenSettings : MonoBehaviour
+{
+
+    public GameObject GameCanvas;
+    public GameObject SettingsMenu;
+
+    public void ShowSettings() {
+        (Instantiate(SettingsMenu) as GameObject).transform.parent = GameCanvas.transform;
+    }
+}
