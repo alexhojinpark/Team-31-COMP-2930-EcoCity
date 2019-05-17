406c569a629bb124ae368c80391d56bfce5d5301
Erik Golke <erikgolke@gmail.com>
2019-05-16 18:23:44 -0700

    Added sounds and fixed some issues

---
+++ Assets/Scripts/Erik Scripts/NukeAudio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NukeAudio : MonoBehaviour
+{
+    public AudioSource nukeSource;
+    public AudioClip[] dontDoIt = new AudioClip[5];
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
